618d0efca0492fdb9440dc3b0eea6a64d316244d
shamsbasir <shamsbasir@gmail.com>
2023-04-16 12:13:37 -0400

    utility function added

---
+++ Convection-Dominated Conv-Diffusion/utility/net_plotter.py
@@ -0,0 +1,185 @@
+"""
+    Manipulate network parameters and setup random directions with normalization.
+"""
+# Ref : https://github.com/tomgoldstein/loss-landscape/blob/master/net_plotter.py
+
+
+import torch 
+
+def get_weights(net):
+    """ Extract parameters from net, and return a list of tensors"""
+    return [p.data for p in net.parameters()]
+
+
+def set_weights(net, weights, directions=None, step=None):
+    """
+        Overwrite the network's weights with a specified list of tensors
+        or change weights along directions with a step size.
+    """
+    if directions is None:
+        # You cannot specify a step length without a direction.
+        for (p, w) in zip(net.parameters(), weights):
+            p.data.copy_(w.type(type(p.data)))
+    else:
+        assert step is not None, 'If a direction is specified then step must be specified as well'
+
+        if len(directions) == 2:
+            dx = directions[0]
+            dy = directions[1]
+            changes = [d0*step[0] + d1*step[1] for (d0, d1) in zip(dx, dy)]
+        else:
+            changes = [d*step for d in directions[0]]
+
+        for (p, w, d) in zip(net.parameters(), weights, changes):
+            p.data = w + torch.Tensor(d).type(type(w))
+            
+
+            
+def set_states(net, states, directions=None, step=None):
+    """
+        Overwrite the network's state_dict or change it along directions with a step size.
+    """
+    if directions is None:
+        net.load_state_dict(states)
+    else:
+        assert step is not None, 'If direction is provided then the step must be specified as well'
+        if len(directions) == 2:
+            dx = directions[0]
+            dy = directions[1]
+            changes = [d0*step[0] + d1*step[1] for (d0, d1) in zip(dx, dy)]
+        else:
+            changes = [d*step for d in directions[0]]
+
+        new_states = copy.deepcopy(states)
+        assert (len(new_states) == len(changes))
+        for (k, v), d in zip(new_states.items(), changes):
+            d = torch.tensor(d)
+            v.add_(d.type(v.type()))
+
+        net.load_state_dict(new_states)
+        
+        
+        
+
+def get_random_weights(weights):
+    """
+        Produce a random direction that is a list of random Gaussian tensors
+        with the same shape as the network's weights, so one direction entry per weight.
+    """
+    return [torch.randn(w.size()) for w in weights]
+
+def get_random_states(states):
+    """
+        Produce a random direction that is a list of random Gaussian tensors
+        with the same shape as the network's state_dict(), so one direction entry
+        per weight, including BN's running_mean/var.
+    """
+    return [torch.randn(w.size()) for k, w in states.items()]
+
+
+def get_diff_weights(weights, weights2):
+    """ Produce a direction from 'weights' to 'weights2'."""
+    return [w2 - w for (w, w2) in zip(weights, weights2)]
+
+
+
+def get_diff_states(states, states2):
+    """ Produce a direction from 'states' to 'states2'."""
+    return [v2 - v for (k, v), (k2, v2) in zip(states.items(), states2.items())]
+
+
+def normalize_direction(direction, weights, norm='filter'):
+    """
+        Rescale the direction so that it has similar norm as their corresponding
+        model in different levels.
+        Args:
+          direction: a variables of the random direction for one layer
+          weights: a variable of the original model for one layer
+          norm: normalization method, 'filter' | 'layer' | 'weight'
+    """
+    if norm == 'filter':
+        # Rescale the filters (weights in group) in 'direction' so that each
+        # filter has the same norm as its corresponding filter in 'weights'.
+        for d, w in zip(direction, weights):
+            d.mul_(w.norm()/(d.norm() + 1e-10))
+    elif norm == 'layer':
+        # Rescale the layer variables in the direction so that each layer has
+        # the same norm as the layer variables in weights.
+        direction.mul_(weights.norm()/direction.norm())
+    elif norm == 'weight':
+        # Rescale the entries in the direction so that each entry has the same
+        # scale as the corresponding weight.
+        direction.mul_(weights)
+    elif norm == 'dfilter':
+        # Rescale the entries in the direction so that each filter direction
+        # has the unit norm.
+        for d in direction:
+            d.div_(d.norm() + 1e-10)
+    elif norm == 'dlayer':
+        # Rescale the entries in the direction so that each layer direction has
+        # the unit norm.
+        direction.div_(direction.norm())
+        
+        
+        
+def normalize_directions_for_weights(direction, weights, norm='filter', ignore='biasbn'):
+    """
+        The normalization scales the direction entries according to the entries of weights.
+    """
+    assert(len(direction) == len(weights))
+    for d, w in zip(direction, weights):
+        if d.dim() <= 1:
+            if ignore == 'biasbn':
+                d.fill_(0) # ignore directions for weights with 1 dimension
+            else:
+                d.copy_(w) # keep directions for weights/bias that are only 1 per node
+        else:
+            normalize_direction(d, w, norm)
+            
+            
+def normalize_directions_for_states(direction, states, norm='filter', ignore='ignore'):
+    assert(len(direction) == len(states))
+    for d, (k, w) in zip(direction, states.items()):
+        if d.dim() <= 1:
+            if ignore == 'biasbn':
+                d.fill_(0) # ignore directions for weights with 1 dimension
+            else:
+                d.copy_(w) # keep directions for weights/bias that are only 1 per node
+        else:
+            normalize_direction(d, w, norm)
+            
+            
+            
+def ignore_biasbn(directions):
+    """ Set bias and bn parameters in directions to zero """
+    for d in directions:
+        if d.dim() <= 1:
+            d.fill_(0)
+
+            
+            
+def create_random_direction(net, dir_type='weights', ignore='biasbn', norm='filter'):
+    """
+        Setup a random (normalized) direction with the same dimension as
+        the weights or states.
+        Args:
+          net: the given trained model
+          dir_type: 'weights' or 'states', type of directions.
+          ignore: 'biasbn', ignore biases and BN parameters.
+          norm: direction normalization method, including
+                'filter" | 'layer' | 'weight' | 'dlayer' | 'dfilter'
+        Returns:
+          direction: a random direction with the same dimension as weights or states.
+    """
+
+    # random direction
+    if dir_type == 'weights':
+        weights = get_weights(net) # a list of parameters.
+        direction = get_random_weights(weights)
+        normalize_directions_for_weights(direction, weights, norm, ignore)
+    elif dir_type == 'states':
+        states = net.state_dict() # a dict of parameters, including BN's running mean/var.
+        direction = get_random_states(states)
+        normalize_directions_for_states(direction, states, norm, ignore)
+
+    return direction
